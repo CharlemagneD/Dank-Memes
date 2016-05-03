64a5d9b004706e5ce5f83327bcf20c965bea26a0
Charlemagne <charlemagne.doles@sjsu.edu>
2016-05-02 23:30:51 -0700

    Removing migration files

--- db/migrate/20160501064844_remove_id_column_from_memes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIdColumnFromMemes < ActiveRecord::Migration
-  def change
-    remove_column :memes, :user_id, :integer
-  end
-end
--- db/migrate/20160501064927_add_user_id_to_memes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToMemes < ActiveRecord::Migration
-  def change
-    add_column :memes, :dank_user_id, :integer
-  end
-end
